e4e78671b7b4df1c5baa03caccac5dea1c57b628
Luke Campbell <luke.s.campbell@gmail.com>
2016-01-08 10:02:45 -0500

    Adds custom IOOS harvesters and better package formatting

---
+++ ckanext-ioos_theme/ckanext/ioos_theme/harvesters/ioos_harvester.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import ckan.plugins as p
+import logging
+from ckanext.spatial.harvesters.base import SpatialHarvester
+from ckanext.spatial.interfaces import ISpatialHarvester
+import json
+
+log = logging.getLogger(__name__)
+
+
+class IOOSHarvester(SpatialHarvester):
+
+    def get_package_dict(self, iso_values, harvest_object):
+        '''
+        '''
+        #for key, value in iso_values.iteritems():
+        #    log.debug("%s: %s", key, value)
+        package_dict = SpatialHarvester.get_package_dict(self, iso_values, harvest_object)
+        extras = {}
+        if iso_values.get('publisher', None):
+            extras['publisher'] = iso_values.get('publisher', [])
+        if iso_values.get('responsible-organisation'):
+            log.info("Checking for responsible-organisation")
+            extras['responsible-organisation'] = iso_values.get('responsible-organisation', [])
+        extras_as_dict = []
+        for key, value in extras.iteritems():
+            if isinstance(value, (list, dict)):
+                extras_as_dict.append({'key': key, 'value': json.dumps(value)})
+            else:
+                extras_as_dict.append({'key': key, 'value': value})
+
+        package_dict['extras'] = package_dict['extras'] + extras_as_dict
+        
+        return package_dict
+
+
